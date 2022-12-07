67ea422ae39fe212ae17e179b1cb9697a8c050b2
Nil Valero <nvalero13@gmail.com>
2022-12-07 13:12:50 +0100

    add first iteration

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <h1>Footer</h1>
+  </footer>
+</template>
+  
+  <script>
+export default {};
+</script>
+  
+  <style>
+footer {
+  height: 100px;
+  width: 100%;
+  background-color: grey;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>Navbar</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+header {
+  height: 100px;
+  width: 100%;
+  background-color: grey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  color: white;
+}
+</style>
